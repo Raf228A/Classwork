9b6728ba5463a6dec9b156301d2f1efec2bc7c48
Raf228A <rafaelavetisan9@gmail.com>
2024-01-25 10:29:01 +0300

    Выполнили работу с многомерными массивами

---
+++ Задание 3/app.js
@@ -0,0 +1,3 @@
+let students = [['Microsoft', 'Apple', 'Linux', 'Huawei', 'Honor'],['McDonalds', 'BurgerKing', 'KFC', 'Wendys', 'Blinberi'],['Magnit', 'METRO', 'Pyaterochka', 'Perekrestok', 'PokupALKO'],['Haier', 'MSI','HP', 'Lenovo', 'Thunderbolt']];
+console.log(students);
+console.log(students[0][1]);
