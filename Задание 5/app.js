0b66fc7bc324a28adbf9af9f04882084682d4e7b
Raf228A <rafaelavetisan9@gmail.com>
2024-01-27 17:07:10 +0300

    Выполнили задание сортировку дыухмерного массива через цикл FOR

--- Задание 5/app.js
+++ Задание 5/app.js
@@ -1,5 +1,15 @@
-const peoples = ["Tom", "Vova", "Sam","Bob", "Nat", "Ivan", "Stas"];
-for(let i = 0; i < peoples.length; i++) {
-let result = peoples[i];
-console.log(result);
+// const peoples = ["Tom", "Vova", "Sam","Bob", "Nat", "Ivan", "Stas"];
+// for(let i = 0; i < peoples.length; i++) {
+// let result = peoples[i];
+// console.log(result);
+// }
+
+// Двухмерный массив
+const peoples = [["Tom", "hady", 39, "ул.пушкина"], ["Bill", "Gaits", 19, "ул.алега"], ["Djon", "Ten", 31, "ул.валеры"],["Tom", "Strengh", 32, "ул.ушкина"],["Tom", "Rhady", 34, "ул.кина"],["Tom", "Dady", 41, "ул.ушкина"],];
+for (let i = 0; i < peoples.length; i++) {
+    for (let j = 0; j < peoples[i].length; j++) {
+        let result = peoples[i][j];
+        console.log(result);
+    }
+    console.log("==========");
 }
