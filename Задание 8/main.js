b43419346a60df674b308bd4f5c9a033c2493855
Raf228A <rafelavetisan9@gmail.com>
2024-02-02 11:20:49 +0300

    Выполнили задание на передачу объекта в стрелочную функцию

---
+++ Задание 8/main.js
@@ -0,0 +1,25 @@
+ const showStudent = (student) => {
+   console.log(`Имя: ${student.name}`);
+    console.log(`Возраст: ${student.age}`);
+    console.log(`Хобби: ${student.hobbies}`);
+    for(let contact in student.contacts) {
+        console.log(`Контакты: ${student.contacts[contact]}`);
+
+    }
+}
+
+const rafDim = {
+    name: "Gomunkul",
+    age: 18,
+    hobbies: ["Играть","Спать","Кушать"],
+    contacts: {
+        phone: ["+79375355564", "+79377374827"],
+        email: ["rafaelavetisan9@gmail.com", "petrenkodimasic@gmail.com"],
+        social: ["@dimchikcc", "@MrProper05(tg)"]
+        
+    }
+    
+        
+}
+
+showStudent(rafDim);
