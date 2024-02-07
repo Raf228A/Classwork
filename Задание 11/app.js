5e46391c67081da944187e0d056ec225e89ff0d8
Raf228A <rafaelavetisan9@gmail.com>
2024-02-07 09:08:49 +0300

    Задание 3

--- Задание 11/app.js
+++ Задание 11/app.js
@@ -1,26 +1,46 @@
-class ArithmeticProgressionSumCalculator {
-    constructor(firstTerm, difference, numberOfTerms) {
-        this.firstTerm = firstTerm;
-        this.difference = difference;
-        this.numberOfTerms = numberOfTerms;
-        this.sum = this.calculateSum();
+class ReshatelKvadratnogoUravnenia {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
-    calculateSum() {
-        if (this.numberOfTerms <= 0){
-            return "Кол-во членов должно быть положительным"
+    reshat() {
+        const discriminant = this.b**2 - 4 * this.a * this.c;
+        if (discriminant > 0) {
+            const pravilo1 = (-this.b + Math.sqrt(discriminant)) / (2 * this.a);
+            const pravilo2 = (-this.b - Math.sqrt(discriminant)) / (2 * this.a);
+            return [pravilo1, pravilo2];
+        } else if (discriminant === 0) {
+            const pravilo = -this.b / (2 * this.a);
+            return pravilo;
+        } else {
+            return null;
         }
-
-        return (this.numberOfTerms / 2) * (2 * this.firstTerm + (this.numberOfTerms - 1) * this.difference);
     }
 
     print() {
-        console.log(`Сумма арифметической прогрессии: ${this.sum}`);
+        const pravila = this.reshat();
+        if (pravila !== null) {
+            if (Array.isArray(pravila)) {
+                console.log("Корни квадратного уравнения:");
+                console.log("x1 =", pravila[0]);
+                console.log("x2 =", pravila[1]);
+            }else {
+                console.log("Корень квадратного уравнения:", pravila);
+            } 
+        } else {
+            console.log("Уравнение не имеет действительных корней");
+        }
     }
 }
 
-const firstTerm = 3;
-const difference = 2;
-const numberOfTerms = 4;
-const progressionSumCalculator = new ArithmeticProgressionSumCalculator(firstTerm, difference, numberOfTerms);
-progressionSumCalculator.print();
+const a = parseFloat(prompt("Введите коэффициент a: "));
+const b = parseFloat(prompt("Введите коэффициент b: "));
+const c = parseFloat(prompt("Введите коэффициент c: "));
+const reshatelUravnenia = new ReshatelKvadratnogoUravnenia(a, b, c);
+reshatelUravnenia.print();
+
+
+
+            
