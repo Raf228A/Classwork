cf547db5b2acec99d5bc190be0ea03f0489c1f14
Raf228A <rafaelavetisan9@gmail.com>
2024-02-13 09:40:46 +0300

    Сделали формулу нахождение кредита

--- Задание 13/app.js
+++ Задание 13/app.js
@@ -1,2 +1,10 @@
-const text = document.querySelector(".block .text");
-console.log(text.innerText);
+const text = document.querySelector(".text")
+const text1 = document.querySelector(".text1")
+let a = text.innerText
+let b = text1.innerText
+let c = a/10*100
+if(a>100000){
+    console.log(`Вы взяли кредит на ${a} и на ${b} года(лет). Сумма задолженности ${c}`)
+} else {
+    console.log("Минимальная сумма взятия кредита должна быть минимум от 100000")
+}
